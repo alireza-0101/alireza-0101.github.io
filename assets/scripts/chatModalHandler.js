dce7cc01dbac7b7292af19ee235d9b5d55d96037
Alireza-MH <alireza.mehdizadeh.84.313@gmail.com>
2025-06-05 20:37:35 +0330

    New Website (NextJS Build)

--- assets/scripts/chatModalHandler.js
+++
@@ -1,8 +0,0 @@
-const body = $.querySelector("body")
-const userIcon = $.querySelector(".user-icon")
-const modalWrap = $.querySelector(".modal-wrap")
-
-const chatModalToggle = () => body.classList.toggle("show-chat-modal")
-
-userIcon.addEventListener("click", chatModalToggle)
-modalWrap.addEventListener("click", chatModalToggle)
